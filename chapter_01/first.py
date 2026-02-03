aa1444a4bb8ab2d0661673616f43d0bcd65e7ee5
SHIVAPRATAP78 <prataptalwar659@gmail.com>
2026-02-03 13:32:46 +0530

    hellowworld.py

---
+++ chapter_01/first.py
@@ -0,0 +1,20 @@
+#  
+
+
+#print function in python 
+
+print("Hello world")
+print ("welcome to python ") 
+
+# new one 
+print("well come to python practice ")
+
+name =input("enter your name: ")
+age =int(input("enter your age: "))
+print ("hello",name) 
+print("next year you will be ",age + 1)
+
+# input statement
+name=input("enter you name ")
+
+print("hello", name )
