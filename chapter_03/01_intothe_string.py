fed4835a2a9c041c27e3dfe4dafe11e91eb2e401
my name <prataptalwar659@gmail.com>
2026-02-06 23:28:12 +0530

    into the strings strings method

---
+++ chapter_03/01_intothe_string.py
@@ -0,0 +1,39 @@
+ # into the strings 
+ #  string is data type in python
+ # string is a sequence of chsrecters enclosed within qoutes
+
+ 
+name = "pratap" # double  qouted string 
+name = 'pratap' #single qouted string 
+name = '''pratap'''# triple qoute string
+
+# string slicing 
+
+name = "pratap"
+
+nameshort= name [0:3]  #start from 0 and end at 3 but not include 3 
+print(nameshort)
+
+nameshort =name [3:6] # start frome 3 and end at 6 but not include 6
+print(nameshort)
+
+name = "pratap"
+nameshort = name [0:2] # start from 0 and end at 2 but not include 2 
+print(nameshort)
+ 
+name ="pratap"
+nameshort = name[0:4]
+print(nameshort)
+
+
+# 
+
+name = "pratap"
+charecter1 = name [1] # it will give the first charecter of the included string 
+print(charecter1)
+
+name = "pratap"
+charecter =name[3] # it will give the third charecter of the included string    
+print(charecter)
+
+
---
+++ chapter_03/02_negative_to_slicing.py
@@ -0,0 +1,12 @@
+name = "pratap"
+
+print(name[0:3])
+
+print(name[-5:-1]) # negative indexung starts from -1 frome the end of the string 
+print(name[1:5])  # postive indexing starts from 0 from the start of the string 
+# negative to positive indexing conversion 
+
+name = "pratap"
+print(name[:5]) #is same as  print name  [0:5]  
+print(name[1:]) # is same as  print name  [1:6]
+print(name[1:6])
