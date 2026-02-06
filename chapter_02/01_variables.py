372272effe3c586a04dca8db1ca632d73837ef6d
SHIVAPRATAP78 <prataptalwar659@gmail.com>
2026-02-07 00:22:11 +0530

    nothing is changed

---
+++ chapter_02/01_variables.py
@@ -0,0 +1,251 @@
+a=1
+b=2
+
+print (a+b)
+c = "partha"
+print(c)
+
+
+
+
+name = "pratap" 
+age = 22
+height = 5.8
+is_student = True 
+
+name = "pratap"
+age =24
+height =6.0
+is_student = False
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+ #box name 
+age  =23 
+print(age)
+
+age= 23
+age =25
+print(age)
+
+a = 10 
+b = 5 
+c = a+b
+print (c)
+
+x=3 
+print(x)
+print(x+3)
+print(x*5)
+  
+# dynamic typing 
+
+data = 10 
+print (data)
+data = "hello "
+print (data)
+
+
+# varible is just a name that stores a value in memory 
+
+age = 24
+name ="pratap"
+
+print (age)
+print (name)
+
+age = 21
+name = "partha"
+salery = 30000
+job = "softwaer devolper "
+
+print (age )
+print (name )
+print (salery)
+print(job)
+
+
+# int - integer (whole numbers)
+# use when counting , age , marks , quantity 
+
+a = 10
+b= -5
+c = 1000
+print (a+b)
+
+# new example
+
+a = 45
+b = -23 
+c = 24 
+print (a+b+c)
+
+# float - decimal numbers 
+
+price = 99,5
+pi = 3,14
+print ( price*2 )
+# new examole 
+price= 10,0
+pi = 11,1
+print (price+pi)
+
+age = 24 
+name = "pratap"
+salary = 30000 
+print (age)
+print(name )
+print(salary)
+
+
+# swiggy order
+order_id=107
+customer_name = "parth" 
+bill_amount = 150 
+print(order_id)
+print(customer_name)
+print(bill_amount)
+
+# addition 
+
+a =10 
+b= 12 
+print (a+b)
+
+#substraction 
+a=15
+b=11
+print(a-b)
+
+#multiplication 
+a=10
+b=3
+print(a*b)
+
+# division  float 
+a=10 
+b= 3
+print(a/b)
+
+#dubdevision integer 
+a=10 
+b=3 
+print(a//b)
+
+#  modulus (reminder)
+a=10
+b=3 
+print(a%b)
+
+# exponentation (power)
+a= 10 
+b= 3
+print(a**b)
+
+ 
+name = input("enter your name;") 
+
+a =10
+b=12
+print(a+b)
+
+# variabels practice 
+#variable is name given to memory 
+#location that stores some values so you can use it later 
+# example
+a = 10 
+b = 2 
+print(a+b)
+
+# 
+username = "Pratap"
+password = "1234" 
+a = input("enter your name: ")
+b = input("enter your password: ")
+
+if username == "pratap" or password == "1234":
+    print ("login successfull")
+
+elif username == "Pratap" or password == "123":
+    print("warning")
+
+else: print ("wrong password enterd")
+#
+username = "pratap"
+password = "1234"    
+if username == "pratap" and password == "1234":
+ print ("login successful")
+else: print("invalid login")
+
+
+
+        #
+item_price = 900
+quantity = 3
+total = item_price*quantity
+
+print("total bill:", total) 
+
+# 
+speed = 60 #mbps
+print("your speed ", speed, "mbps")
+
+# 
+api_url = "https pratap. com "
+print(api_url)  
+
+#
+
+sender = "Pratap" 
+message = "hi bro"
+print (sender,"says;" ,message)
+# 
+sender ="shreya"
+message = "hi pratap"
+print(sender, "says;" , message)
+
+# 
+
+server_status = True 
+cpu_usage= 80
+ 
+if cpu_usage>75:
+    print ("high cpu usage!") 
+
+# 
+username = "pratap"
+password = "admin123"
+
+print(username+password)
+
+#
+followers = 930
+followers =  followers +30
+print(" followers" , followers) 
+
+
+username =input ("enter username :")
+password =input ("enter password :")    
+if username == "pratap" and password == "1234":
+ print ("login successful")
+else: print("invalid login")
+
+# 
+username= input("enter your username")
+if username == "pratap": 
+ print("wellcome",username)
+else:print("wrong user")
+
+# 
+
+username =input(" enter username")
+if username == "pratap":
+ print("wellcome python devoloper")
+else: print("wrong user")
+
+a = (12)
+
+b = (13)
+print(a+b)
---
+++ chapter_02/02_datatypes.py
@@ -0,0 +1,24 @@
+a =1 # a is a integer 
+
+b =5.2 # b is a floating point number 
+
+c ="parth" # c is a string 
+
+d = False # is a boolean varaible 
+
+e = None # e is a none type variable
+
+#data types 
+
+age = 23 #int 
+name = "pratap" #string
+price = 23.4 #floating point number
+is_student = True #boolean 
+
+#input and output 
+
+
+age = 23
+name= "pratap"
+height = 5.8
+is_boy = True
