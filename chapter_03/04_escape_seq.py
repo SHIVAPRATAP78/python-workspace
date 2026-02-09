983fcf3e1ac27f6d7ad87499fd68f4d2b7e3c705
SHIVAPRATAP78 <prataptalwar659@gmail.com>
2026-02-09 22:50:18 +0530

    escape sequence

---
+++ chapter_03/04_escape_seq.py
@@ -0,0 +1,18 @@
+# Escape Sequence
+# \n - new line
+
+a = "batman is a super hero and \n he is very \"strong\"" 
+print(a)
+
+# \t - tab space
+
+a = "batman is a super hero and \t he is very  strong"
+print(a)
+
+# \\ - backslash
+a = "batman is a super hero \\ and he is very strong"
+print(a)
+
+# \' - single quote
+a = 'batman is a super hero and he is very strong \'and he is very strong\''
+print(a)
--- chapter_03_ps/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-height = input ("enter your height in , cm ")
-weight =input("enter your weight in , kg")
-print(f"your height is {height}cm and your weight is {weight}kg")
-
-
