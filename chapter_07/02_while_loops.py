7be8f22642f2b04b9b00277df013e6dbb7e4f5e6
SHIVAPRATAP78 <prataptalwar659@gmail.com>
2026-02-21 21:29:44 +0530

    list using while and for loops

--- chapter_07/02_while_loops.py
+++ chapter_07/02_while_loops.py
@@ -1,5 +1,15 @@
 i = 1
-while(i<6):
+while(i<51):
     print(i)
     i+=1
+# 
+i = 0
+while i < 10:
+    print("partha")
+i=i+1
 
+#
+i = 0
+while i < 5:
+    print("batman")
+i=i+1
---
+++ chapter_07/03_listusing_while.py
@@ -0,0 +1,5 @@
+l =[1, "partha ",False,"this","batman","superman","ironman" ]
+i = 0
+while(i<len(l)):
+    print(l[i])
+    i+=1
