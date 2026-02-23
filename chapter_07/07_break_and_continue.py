73b428407445acb0c5701a61d214dd4f16aed88b
SHIVAPRATAP78 <prataptalwar659@gmail.com>
2026-02-23 19:09:07 +0530

    break and continue

---
+++ chapter_07/07_break_and_continue.py
@@ -0,0 +1,15 @@
+for i in range (100):
+    print (i)
+
+###
+
+for i in range (100):
+    if(i ==34):
+        break #exit the loop right now
+    print (i)
+
+
+for i in range (100):
+    if(i ==34):
+        continue # SKIP itreation 
+    print (i)
